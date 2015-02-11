1beeb07cb01e73cacedb541b75eca422940d1ddf
D-ea <dealiskaryani@gmail.com>
2015-02-11 11:47:08 +0700

    Report 9

---
+++ application/modules/reports/controllers/dp_cash_advance_summry.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class dp_cash_advance_summry extends CI_Controller {	
+	public function __construct()
+	{
+		parent::__construct();
+		//$this->load->model('Authentication');
+	}
+	
+	public function home(){
+		
+
+		$log = $this->session->all_userdata();
+		$userLogged = $this->session->userdata('userLogged');
+		
+		if ($userLogged) {
+			$content = array (
+				"log" => $log,
+				"base_url" => base_url(),
+			);
+			$this->twig->display("dp_cash_advance_summry", $content);
+		}
+	}
+	
+		
+}
+?>
---
+++ application/modules/reports/controllers/stock_invoice_voucher.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class stock_invoice_voucher extends CI_Controller {	
+	public function __construct()
+	{
+		parent::__construct();
+		//$this->load->model('Authentication');
+	}
+	
+	public function home(){
+		
+
+		$log = $this->session->all_userdata();
+		$userLogged = $this->session->userdata('userLogged');
+		
+		if ($userLogged) {
+			$content = array (
+				"log" => $log,
+				"base_url" => base_url(),
+			);
+			$this->twig->display("stock_invoice_voucher", $content);
+		}
+	}
+	
+		
+}
+?>
